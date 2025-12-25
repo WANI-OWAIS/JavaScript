3ce5697ba30b253a83489c40c31ea3f22286f1d0
WANI-OWAIS <waniowais528@gmail.com>
2025-12-25 14:13:55 +0530

    p1

---
+++ p1/script.js
@@ -0,0 +1,26 @@
+// 1. Declare your name and city using const, and your age using let 
+// const name = "Owais"
+// const city = "Gulamarg"
+// let age = 21
+
+// 2. Try this and observe the result:
+// let x = 5;
+// let x = 10;
+
+// 3. Guess the output:
+// console.log(count);  
+// var count = 42;
+
+// 4. Create a const object and add a new key to it — does it work?
+// const student = {
+//     name: "Owais"
+// }
+// student.age = 21;
+
+// 5. Try accessing a let variable before declaring it — what error do you see?
+// console.log(age);
+// let age = 21;
+
+// 6. Change a const array by pushing a value. Will it throw an error?
+// const arr = [1,2,3,4,5];
+// arr.push(6);
