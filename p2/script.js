738714d55b2bcd66fcc2574f016024bd250461b1
WANI-OWAIS <waniowais528@gmail.com>
2025-12-25 15:37:42 +0530

    p2

---
+++ p2/script.js
@@ -0,0 +1,32 @@
+// Practice Zone
+
+// 1. Predict the output:
+// console.log(null + 1);
+// console.log("5" + 3);
+// console.log("5" - 3);
+// console.log(true + false);
+
+// 2. Check types:
+// console.log(typeof []);
+// console.log(typeof null);
+// console.log(typeof 123n);
+
+// 3. Truthy or Falsy?
+// console.log(Boolean(0));
+// console.log(Boolean("0"));
+// console.log(Boolean([]));
+// console.log(Boolean(undefined));
+
+// 4. Write a function isEmpty(value) that checks if a given value is 
+// function isEmpty(value) {
+//      return value === null || value === undefined || value === "";
+// }
+
+// isEmpty("");   
+// isEmpty(null);     
+// isEmpty(undefined); 
+// isEmpty("Hi"); 
+
+// 5. Compare with loose vs strict:
+// console.log(5 == "5");
+// console.log(5 === "5");
