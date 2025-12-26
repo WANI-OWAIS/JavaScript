61a9f44e9304eeb12d179f9fd1a15b576778272f
WANI-OWAIS <waniowais528@gmail.com>
2025-12-26 23:45:06 +0530

    p3

---
+++ p3/script.js
@@ -0,0 +1,45 @@
+// 🧪 Practice Zone
+// 1. Predict:
+// console.log("10" + 1);
+// console.log("10" - 1);
+// console.log(true + false);
+// console.log(!!"Sheryians");
+
+// 2. Convert using unary +
+// let str = "42";
+// let num = +str;
+// console.log(num);
+
+// 3. Use ternary:
+// let age = 17;
+// let msg = age >= 18 ? "Adult" : "Minor";
+// console.log(msg);
+
+
+// 4. Build a calculator:
+// let ans;
+// function calc(a, b, operator) {
+//     switch (operator) {
+//         case "Add":
+//             ans = a + b;
+//             break;
+//         case "Sub":
+//             ans = a - b;
+//             break;
+//         case "Mul":
+//             ans = a * b;
+//             break;
+//         case "Div":
+//             ans = a / b;
+//             break;
+//     }
+//     return ans;
+// }
+
+// let res = calc(2, 3, "Add");
+// console.log(res);
+
+// 5. Score logic:
+let marks = 82;
+let res = marks >= 80 ? "Excellent" : marks >= 60 ? "Good" : marks >= 40 ? "Average" : "Fail";
+console.log(res);
