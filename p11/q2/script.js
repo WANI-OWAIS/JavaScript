66d801ca64a736f58e8a7f28cf41fd1554501f97
WANI-OWAIS <waniowais528@gmail.com>
2026-02-22 22:36:36 +0530

    q2

--- p11/q2/script.js
+++ p11/q2/script.js
@@ -1,17 +1,18 @@
 let form = document.querySelector("form");
-let inps = document.querySelectorAll("input");
+let inp = document.querySelectorAll("input");
 
 form.addEventListener("submit", function(dets){
-   dets.preventDefault();
+    dets.preventDefault();
+     
+    let h3 = document.createElement("h3");
+    h3.textContent = "Submitted Data";
+    document.body.appendChild(h3);
 
-let h3 = document.createElement("h3");
- h3.textContent = "Submitted Data :"
- document.body.appendChild(h3);
-
-inps.forEach(function(input){
-let p = document.createElement("p");
-p.textContent = input.value;
-document.body.appendChild(p); 
-})
-
+    inp.forEach(function(input){
+        if(input.type !== "submit"){
+            let p = document.createElement("p");
+            p.textContent = input.value;
+            document.body.appendChild(p);
+        }
+    });
 });
