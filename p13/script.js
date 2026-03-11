6bc6f26ae1545303ac79e208425ef15be09cfbd7
WANI-OWAIS <waniowais528@gmail.com>
2026-03-12 01:26:43 +0530

    P13

---
+++ p13/script.js
@@ -0,0 +1,45 @@
+// //localStorage  ~5Mb
+// //store
+// localStorage.setItem("name", "Owais");
+
+// //fetch
+// let val = localStorage.getItem("name");
+
+// //remove
+// localStorage.removeItem("name");
+
+// //update
+// localStorage.setItem("name", "Wani Owais");
+
+// //clear
+// localStorage.clear();
+
+
+// //sessionStorage
+// //store
+// sessionStorage.setItem("name", "Owais");
+
+// //fetch
+// let vall = sessionStorage.getItem("name");
+
+// //remove
+// sessionStorage.removeItem("name");
+
+// //update
+// sessionStorage.setItem("name", "Wani Owais");
+
+// //clear
+// sessionStorage.clear();
+
+
+// //Cookies  ~4Kb
+// document.cookie = "email=owais@gmail.com";
+// document.cookie = "age=21";
+
+//localStorage mai array wagara store nhi karsakte is liyay unhay string bankar store karte hai 
+// localStorage.setItem("Fruits", JSON.stringify(["Apple","Banana","Orange"]));
+
+//and jab ls se data waps niklatay hai to string milta hai ab string ise apne natural form ma lanay kaliyay JSON.parse karna padta hai
+// JSON.parse("[\"Apple\",\"Banana\",\"Orange\"]");
+
+
