7ee1d764ae54cff66993b0690d10e0cff9d8045a
WANI-OWAIS <waniowais528@gmail.com>
2026-08-11 00:09:25 +0530

    Variables & Declarations

---
+++ Chapter 1 - Variables & Declarations/script.js
@@ -0,0 +1,30 @@
+// 1. Declare your name and city using const , and your age using let.
+// const name = "ovais";
+// const city = "New York";
+// let age = 21;
+
+// 2. Try this and observe the result:
+// let x = 5;
+// let x = 10;
+
+// 3. Guess the output:
+// console.log(count); //undefined
+// var count = 42;  
+
+// 4. Create a const object and add a new key to it — does it work?
+// const person = {
+//   name: "John"
+// };
+
+// person.age = 25;
+
+// console.log(person);
+
+// 5. Try accessing a let variable before declaring it — what error do you see?
+// console.log(a);
+// let a = 21;
+
+// 6. Change a const array by pushing a value. Will it throw an error?
+// const fruits = ["apple", "banana"];
+// fruits.push("orange");
+// console.log(fruits);
